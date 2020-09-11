cdc147f48eb4830ed8c7649d07a7883a3a81f612
yuichi oyama <yuichi19991208@gmail.com>
2020-09-11 11:14:38 +0900

    test.rb修正

--- test.rb
+++ test.rb
@@ -7,6 +7,7 @@ server = WEBrick::HTTPServer.new({
 ['INT', 'TERM'].each {|signal|
   Signal.trap(signal){ server.shutdown }
 }
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'web.html.erb')
 server.mount('/web', WEBrick::HTTPServlet::ERBHandler, 'web.html.erb')
 server.mount('/webgoya_one.cgi', WEBrick::HTTPServlet::CGIHandler, 'webgoya_one.rb')
 server.mount('/webgoya_two.cgi', WEBrick::HTTPServlet::CGIHandler, 'webgoya_two.rb')
